4035005e56f8097a17194abb78b04c7c724cab8c
Alexander Leahy <alex@languagetub.com>
2022-06-11 17:51:03 +1000

    added error message to upload images

--- src/components/AddImageForm.vue
+++ src/components/AddImageForm.vue
@@ -7,6 +7,7 @@
           <input type="text" v-model="imageUri" class="border px-2 py-1 shadow w-full rounded" id="image_uri" name="image_uri">
         </label>
       </div>
+      <ErrorMessage field="image_uri" class="mt-2"/>
       <PrimaryButton type="submit" class="mt-4">Submit Image</PrimaryButton>
     </form>
 
@@ -17,6 +18,7 @@
 </template>
 <script setup>
 import PrimaryButton from "./PrimaryButton.vue";
+import ErrorMessage from "./ErrorMessage.vue";
 import {ref, watch } from "vue";
 import {useErrorStore} from "../stores/useError";
 import {useImageStore} from "../stores/useImages";
@@ -24,17 +26,31 @@ import {useImageStore} from "../stores/useImages";
 const imageUri = ref('');
 const displayImage = ref(false);
 const imageStore = useImageStore();
-const errors = useErrorStore();
+const errorsStore = useErrorStore();
 
 const hideImage = () => {
   displayImage.value = false;
 };
 
+const resetForm = () => {
+  imageUri.value = "";
+};
+
 const onSubmit = () => {
-  imageStore.postImageUri(imageUri.value)
-    .then()
+  errorsStore.$reset();
+
+  if (!displayImage.value) {
+    errorsStore.setErrorForField('image_uri', 'This is not a valid image.');
+    return;
+  }
+
+  imageStore
+    .postImageUri(imageUri.value)
+    .then(() => {
+      resetForm();
+    })
     .catch(({response}) => {
-      errors.storeErrorsResponse(response);
+      errorsStore.storeErrorsResponse(response);
     });
 };
 
--- src/stores/useError.js
+++ src/stores/useError.js
@@ -10,5 +10,9 @@ export const useErrorStore = defineStore("error", {
       this.errors = response?.data?.errors ?? {};
       this.message = response?.data?.message ?? null;
     },
+    setErrorForField(field, message) {
+      this.errors = {};
+      this.errors[field] = [message];
+    },
   },
 });
